a8deda03c496257d4ca679748cb06db155b648e4
Exinker <exinker@gmail.com>
2024-03-11 14:55:31 +0700

    release/0.1.6

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
+__version__ = '0.1.6'
+
+
 APPLICATION_NAME = 'Atom Information Measurement System'
 APPLICATION_DESCRIPTION = ''''''
-APPLICATION_VERSION = '0.1.5'
+APPLICATION_VERSION = __version__
 
 AUTHOR_NAME = 'Pavel Vaschenko'
 AUTHOR_EMAIL = 'vaschenko@vmk.ru'
